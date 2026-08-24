3ec0726908991b166e8c36d63a86c239ed4ef73a
Daniel <daniel.palmen@gmail.com>
2026-08-24 09:08:02 +0300

    update 1 file and create 1 file

--- ex4.c
+++ ex4.c
@@ -1,13 +1,35 @@
 #include <stdio.h>
 
-int main(){
-    printf("Enter positive numbers or negative to stop: ");
-    int read_integer(void);
-    printf(read_integer());
-}
-
 int read_integer(void){
     int number;
     scanf("%d", &number);
-    return number;
+    if(number >= 1){
+        return number;
+    }
+    else if(number < 0){
+        /*end clause*/
+        return 0;
+    }
+    else{
+        printf("invalid input.");
+        return 0;
+    }
+}
+
+int main(){
+    int sum;
+    int counter;
+    float average;
+    printf("Enter positive numbers or negative to stop: ");
+    int number = read_integer();
+    if(number > 0){
+        counter++;
+        sum = sum + number;
+        average = sum / counter;
+    }
+    else if(number < 0){
+        //*end loop
+    }
+    
+    printf("You entered %d positive numbers. The average is: %.2f", number, average);
 }
