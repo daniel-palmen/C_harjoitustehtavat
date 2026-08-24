2c7f3da9f51c1f886376b87f2196b5662c5753bf
Daniel <daniel.palmen@gmail.com>
2026-08-24 11:41:11 +0300

    update ex7.c

---
+++ Oma_ex/ex7.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+bool read_positive(int *value);
+
+int main(){
+    int value;
+    int counter = 0;
+    int money;
+    bool entry;
+    while(counter < 3){
+        printf("Guess how much money I have!\n");
+        entry = read_positive(&value);
+        if(entry == 1){
+            money = 2 * value + 20;
+            printf("You didn't get it right. I have %d euros.\n", money);
+            counter ++;
+        }
+    }
+
+}
+
+bool read_positive(int *value){
+    int result;
+    int number;
+    printf("Enter a positive number: ");
+    result = scanf("%d", &number);
+    if(result != 1){
+        printf("Incorrect input\n");
+        while(getchar() != '\n');
+        return 0;
+    }
+    else{
+        *value = number;
+        return 1;
+    }
+}
