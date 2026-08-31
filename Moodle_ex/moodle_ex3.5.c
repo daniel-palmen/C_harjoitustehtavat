925ab0e4d8dc53d8d384c5362301fd83f79d7f72
Daniel <daniel.palmen@gmail.com>
2026-08-31 12:58:51 +0300

    update moodle_ex3.5.c and moodle_ex3.5.exe

--- Moodle_ex/moodle_ex3.5.c
+++ Moodle_ex/moodle_ex3.5.c
@@ -1,20 +1,47 @@
 #include <stdio.h>
 
-int main(){
-    char raw_line[100];
-    int value_1;
-    int value_1_size;
-    int value_2;
-    int value_2_size;
-    char *command;
-    fgets(raw_line, 100, stdin);
-    printf(raw_line);
-    for(int i = 0; i < raw_line; i++){
-        if(raw_line[i] != ' '){
-            value_1_size ++;
+int main() {
+    char raw_line[20];
+    char new_line[20];
+    char command_line[20];
+    int j = 0;
+    int k = 0;
+    int start = 0;
+    int value_1 = 0;
+    int value_2 = 0;
+    int result = 0;
+    fgets(raw_line, 20, stdin);
+    sscanf(raw_line, "%d", &value_1);
+    for(int i = 0; raw_line[i] != '\0'; i++){
+        if(raw_line[i] == ' '){
+            start++;
         }
-
+        if(start == 1){
+            new_line[j] = raw_line[i];
+            j++;
+        }
+        if(start == 2 && raw_line[i] != ' '){
+            command_line[k] = raw_line[i];
+            k++;
+        }
     }
 
-    printf("value 1 size: %d", value_1_size);
+    new_line[j] = '\0';
+    command_line[k] = '\0';
+
+    sscanf(new_line, "%d", &value_2);
+
+    if(command_line[0] == 'a' &&
+    command_line[1] == 'd' &&
+    command_line[2] == 'd'){
+        result = value_1 + value_2;
+        printf("%d\n", result);
+    }
+    else if(command_line[0] == 's' &&
+    command_line[1] == 'u' &&
+    command_line[2] == 'b'){
+        result = value_1 - value_2;
+        printf("%d\n", result);
+    }
+    else if (command_line[0] == 'd')
 }
