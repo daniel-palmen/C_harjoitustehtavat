bbfe71b4f0f13d50deaba2d1e82d2288941c5992
Daniel <daniel.palmen@gmail.com>
2026-09-07 09:20:14 +0300

    update moodle_ex3.5.c and moodle_ex3.5.exe

--- Moodle_ex/moodle_ex3.5.c
+++ Moodle_ex/moodle_ex3.5.c
@@ -10,8 +10,10 @@ int main() {
     int value_1 = 0;
     int value_2 = 0;
     int result = 0;
+    int check_1 = 0;
+    int check_2 = 0;
     fgets(raw_line, 20, stdin);
-    sscanf(raw_line, "%d", &value_1);
+    check_1 = sscanf(raw_line, "%d", &value_1);
     for(int i = 0; raw_line[i] != '\0'; i++){
         if(raw_line[i] == ' '){
             start++;
@@ -24,24 +26,49 @@ int main() {
             command_line[k] = raw_line[i];
             k++;
         }
+        if(raw_line[i] == '\n'){
+            raw_line[i] = '\0';
+        }
     }
 
     new_line[j] = '\0';
     command_line[k] = '\0';
 
-    sscanf(new_line, "%d", &value_2);
-
-    if(command_line[0] == 'a' &&
+    check_2 = sscanf(new_line, "%d", &value_2);
+    if(check_1 != 1){
+        printf("Input: \"%s\" Invalid argument in position: 1", raw_line);
+    }
+    else if(check_2 != 1){
+        printf("Input: \"%s\" Invalid argument in position: 2", raw_line);
+    }
+    else if(k > 4){
+        printf("Input: \"%s\" Unknown command: %s", raw_line, command_line);
+    }
+    else if(command_line[0] == 'a' &&
     command_line[1] == 'd' &&
     command_line[2] == 'd'){
         result = value_1 + value_2;
-        printf("%d\n", result);
+        printf("%d + %d = %d\n", value_1, value_2, result);
     }
     else if(command_line[0] == 's' &&
     command_line[1] == 'u' &&
     command_line[2] == 'b'){
         result = value_1 - value_2;
-        printf("%d\n", result);
+        printf("%d - %d = %d\n", value_1, value_2, result);
     }
-    else if (command_line[0] == 'd')
+    else if(command_line[0] == 'm' &&
+    command_line[1] == 'u' &&
+    command_line[2] == 'l'){
+        result = value_1 * value_2;
+        printf("%d * %d = %.d\n", value_1, value_2, result);
+    }
+    else if(command_line[0] == 'd' &&
+    command_line[1] == 'i' &&
+    command_line[2] == 'v'){
+        result = value_1 / value_2;
+        printf("%d / %d = %d\n", value_1, value_2, result);
+    }
+    else{
+        printf("Input: \"%s\" Unknown command: %s", raw_line, command_line);
+    }
 }
