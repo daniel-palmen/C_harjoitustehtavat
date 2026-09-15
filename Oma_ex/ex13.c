e5104eedd3f74766a612c39f5c692828e7d7d308
Daniel <daniel.palmen@gmail.com>
2026-09-15 09:58:25 +0300

    update 2 files and create 1 file

--- Oma_ex/ex13.c
+++ Oma_ex/ex13.c
@@ -1,16 +1,50 @@
 #include <stdio.h>
+#define LINE_SIZE 80
 
-main(){
-    char name[100];
+int main(){
+    char name[LINE_SIZE];
+    char line[LINE_SIZE];
+    int number = 0;
+    int largest = 0;
+    int smallest = 0;
+    int count = 0;
+    int first_line = 0;
     FILE *file;
     printf("Enter file name: ");
-    fgets(name, 100, stdin);
+    fgets(name, LINE_SIZE, stdin);
+    for(int i = 0; name[i] != '\0'; i++){
+        if(name[i] == '\n'){
+            name[i] = '\0';
+        }
+    }
     file = fopen(name, "r");
     if(file == NULL){
         //error message
+        fprintf(stderr, "Opening file failed.\n");
     }
     else{
-        
-    }
+        while(!feof(file)){
+            if(fgets(line, LINE_SIZE, file) != NULL){
+                if(sscanf(line, "%d", &number) == 1){
+                    if(first_line == 0){
+                        largest = number;
+                        smallest = number;
+                        first_line = 1;
+                    }
+                    if(number > largest){
+                        largest = number;
+                    }
+                    if(number < smallest){
+                        smallest = number;
+                    }
+                    count++;
 
+                }
+            }
+
+        }
+        printf("Count of numbers: %d\n", count);
+        printf("Smallest number: %d\n", smallest);
+        printf("Largest number: %d\n", largest);
+    }
 }
